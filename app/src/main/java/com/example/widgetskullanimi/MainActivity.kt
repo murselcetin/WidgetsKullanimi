fc9aaaf5345ad1145d7bc623cdb3be1350fea842
Mürsel <murselcetin51@gmail.com>
2022-06-19 21:48:36 +0300

    Ders 7 işlendi.

--- app/src/main/java/com/example/widgetskullanimi/MainActivity.kt
+++ app/src/main/java/com/example/widgetskullanimi/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.widgetskullanimi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.widgetskullanimi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonResim1.setOnClickListener {
+            binding.imageView.setImageResource(R.drawable.resim1)
+        }
+
+        binding.buttonResim2.setOnClickListener {
+            binding.imageView.setImageResource(
+                resources.getIdentifier(
+                    "resim2",
+                    "drawable",
+                    packageName
+                )
+            )
+        }
+
     }
 }
